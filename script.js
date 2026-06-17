3b2a38e3cd77e2cd3506583290533886a8c0d563
premium-hn <soyjulissadiazhn@gmail.com>
2026-06-16 23:26:36 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,216 @@
+const CANALES_DATOS = [
+  {
+    "id": 1,
+    "nombre": "Brisna Reyes",
+    "descripcion": "Comunidad premium oficial de Brisna Reyes. Acceso verificado y actualizaciones diarias.",
+    "categoria": "Más Buscadas",
+    "pais": "HONDUREÑAS",
+    "imagen": "https://lh3.googleusercontent.com/d/1H4k6rbM3B1BWb4FI6ZlpZbfUB-0Xg3v_",
+    "enlace": "https://t.me/+C2dX4D28U244ZDZh"
+  },
+  {
+    "id": 2,
+    "nombre": "Kensy Solis",
+    "descripcion": "Canal verificado oficial de Kensy Solis. Únete al grupo con las publicaciones exclusivas.",
+    "categoria": "Más Buscadas",
+    "pais": "HONDUREÑAS",
+    "imagen": "https://lh3.googleusercontent.com/d/1CHWs_2JZICXvnYMg8kozn4PFdR8e0Q3b",
+    "enlace": "https://t.me/+rtSZIUKSkVBlNzIx"
+  },
+  {
+    "id": 3,
+    "nombre": "Valeria Aguilar",
+    "descripcion": "Comunidad privada oficial de Valeria Aguilar en Telegram. Todo el contenido en alta calidad.",
+    "categoria": "Más Buscadas",
+    "pais": "HONDUREÑAS",
+    "imagen": "https://lh3.googleusercontent.com/d/1NwgfTAi0IvHpOqKSgpMFre9T_RfTB-Y3",
+    "enlace": "https://t.me/+A0xn0NVjVuRjNjYx"
+  },
+  {
+    "id": 4,
+    "nombre": "Desire Diaz",
+    "descripcion": "Canal premium exclusivo verificado de Desire Diaz. El espacio oficial con novedades constantes.",
+    "categoria": "Más Buscadas",
+    "pais": "HONDUREÑAS",
+    "imagen": "https://lh3.googleusercontent.com/d/1B1-jKbJcO-9jvzU6_yEUrXiBhK0k3908",
+    "enlace": "https://t.me/+MmIkwb1PoMk0Nzgx"
+  },
+  {
+    "id": 5,
+    "nombre": "Step Reyes",
+    "descripcion": "Grupo oficial de Step Reyes. Espacio VIP activo con actualizaciones diarias en alta velocidad.",
+    "categoria": "Más Buscadas",
+    "pais": "HONDUREÑAS",
+    "imagen": "https://lh3.googleusercontent.com/d/1J6yXiUwuRxJWkNTk3cNp2VuMVqf1PpvE",
+    "enlace": "https://t.me/+H_am9c5lam00ZDgx"
+  },
+  {
+    "id": 6,
+    "nombre": "La Niche",
+    "descripcion": "Únete para conocer las últimas publicaciones dentro de la comunidad oficial de La Niche.",
+    "categoria": "Más Buscadas",
+    "pais": "HONDUREÑAS",
+    "imagen": "https://lh3.googleusercontent.com/d/1Pm-35B2KPfvdonKcCuYqdnQ9naI74kJ0",
+    "enlace": "https://t.me/+SrhaczXTjJs2ZmFh"
+  },
+  {
+    "id": 7,
+    "nombre": "Jelen Santos",
+    "descripcion": "Canal exclusivo de Jelen Santos. Todo el contenido oficial de forma premium.",
+    "categoria": "Más Buscadas",
+    "pais": "HONDUREÑAS",
+    "imagen": "https://lh3.googleusercontent.com/d/11qgUGCUEfQX2U3cbTn4tq7bQxgRD7VbB",
+    "enlace": "https://t.me/+0EZ6euY5wYE2MWQx"
+  },
+  {
+    "id": 8,
+    "nombre": "Meylin Cardenas",
+    "descripcion": "Comunidad oficial verificada de Meylin Cardenas. Acceso directo al directorio privado.",
+    "categoria": "Más Buscadas",
+    "pais": "HONDUREÑAS",
+    "imagen": "https://lh3.googleusercontent.com/d/1emzU7q1hYpqorUFWMwkHoThowqICXWB0",
+    "enlace": "https://t.me/+2P1Wb7MxgMxlNzIx"
+  },
+  {
+    "id": 9,
+    "nombre": "Mary Guerra",
+    "descripcion": "Accede al canal oficial de Mary Guerra. Actualizaciones exclusivas y contenido verificado.",
+    "categoria": "Más Buscadas",
+    "pais": "HONDUREÑAS",
+    "imagen": "https://lh3.googleusercontent.com/d/13Xk1eyiBFz1XoEFi3rIFo93c0p6NITuQ",
+    "enlace": "https://t.me/+fo_lHhph4iRmYzMx"
+  },
+  {
+    "id": 10,
+    "nombre": "jassmin Abrego",
+    "descripcion": "Espacio premium VIP para miembros del canal oficial de jassmin Abrego.",
+    "categoria": "Más Buscadas",
+    "pais": "HONDUREÑAS",
+    "imagen": "https://lh3.googleusercontent.com/d/1WeaWaC00h4m6pIWVhxvIxaHNKYGB5o7L",
+    "enlace": "https://t.me/+k_FlZQM4ZQA4MjQx"
+  },
+  {
+    "id": 11,
+    "nombre": "Zamy Gaibor",
+    "descripcion": "Canal principal verificado de Zamy Gaibor. Acceso inmediato a sus mejores publicaciones.",
+    "categoria": "Más Buscadas",
+    "pais": "HONDUREÑAS",
+    "imagen": "https://lh3.googleusercontent.com/d/1c-pYQNWVhqv--zFwerf3_GSE6cnJEBF7",
+    "enlace": "https://t.me/+2xvJ4qhEhwpjMWMx"
+  },
+  {
+    "id": 12,
+    "nombre": "Lopez Herrera",
+    "descripcion": "Comunidad dedicada oficial de Lopez Herrera con las actualizaciones más recientes.",
+    "categoria": "Más Buscadas",
+    "pais": "HONDUREÑAS",
+    "imagen": "https://lh3.googleusercontent.com/d/19QqKty0QXpVMdI0jnTmXeBwipMvg16Y1",
+    "enlace": "https://t.me/+U95RBZL1x5wxMmYx"
+  },
+  {
+    "id": 13,
+    "nombre": "Yanny Fonseca",
+    "descripcion": "Canal exclusivo de Yanny Fonseca. Todo el contenido de forma rápida y directa.",
+    "categoria": "Más Buscadas",
+    "pais": "HONDUREÑAS",
+    "imagen": "https://lh3.googleusercontent.com/d/19KDhiJ7SrxmYggwQ8I8Gc9VolENR-Ub5",
+    "enlace": "https://t.me/+fX9BmUO3vL00YTMx"
+  },
+  {
+    "id": 14,
+    "nombre": "Sofia Pineda",
+    "descripcion": "Comunidad oficial verificada de Sofia Pineda. Acceso libre al directorio de publicaciones.",
+    "categoria": "Más Buscadas",
+    "pais": "HONDUREÑAS",
+    "imagen": "https://lh3.googleusercontent.com/d/1wM0RIRLY_BidL2F00X1X-wQ6STCabNh4",
+    "enlace": "https://t.me/+WcHgGCPPa4gyY2Q5"
+  },
+  {
+    "id": 15,
+    "nombre": "Mia Millón",
+    "descripcion": "Acceso premium al canal oficial de Mia Millón. Únete al grupo verificado.",
+    "categoria": "Más Buscadas",
+    "pais": "HONDUREÑAS",
+    "imagen": "https://lh3.googleusercontent.com/d/1iGmGuOg30ocAzoauMusUGLkYShtFSiql",
+    "enlace": "https://t.me/+1blGKmu2rLo3N2Vh"
+  },
+  {
+    "id": 16,
+    "nombre": "Mi Flaca",
+    "descripcion": "Comunidad privada oficial de Mi Flaca con el contenido y novedades más buscadas.",
+    "categoria": "Más Buscadas",
+    "pais": "HONDUREÑAS",
+    "imagen": "https://lh3.googleusercontent.com/d/1cigASuODbF6M-rcao8q8iFef9BDO9pPQ",
+    "enlace": "https://t.me/+Lrd-I_FoJPEzMjIx"
+  },
+  {
+    "id": 17,
+    "nombre": "Vanessa Barahona",
+    "descripcion": "Canal oficial verificado de Vanessa Barahona. Actualizaciones completas y acceso directo.",
+    "categoria": "Más Buscadas",
+    "pais": "HONDUREÑAS",
+    "imagen": "https://lh3.googleusercontent.com/d/1086LH_gBxbkhYJ96II7M5D7qgyxJkEQp",
+    "enlace": "https://t.me/+Z0-IqDIG3gBlMGUx"
+  },
+  {
+    "id": 18,
+    "nombre": "Sindy Lopez",
+    "descripcion": "Grupo premium de Sindy Lopez. Conéctate directamente para no perderte ninguna novedad.",
+    "categoria": "Más Buscadas",
+    "pais": "HONDUREÑAS",
+    "imagen": "https://lh3.googleusercontent.com/d/1-a07h8pjZ_U-G_Uw3koNRPuIUDLckpCu",
+    "enlace": "https://t.me/+A6hV7VDZ4hJhZGEx"
+  },
+  {
+    "id": 19,
+    "nombre": "Nala Rodríguez",
+    "descripcion": "Canal principal verificado de Nala Rodríguez. Acceso inmediato a sus mejores publicaciones.",
+    "categoria": "Más Buscadas",
+    "pais": "HONDUREÑAS",
+    "imagen": "https://lh3.googleusercontent.com/d/1EFlCzT8nmNDNcA9k901fSDMYu1cL9VtZ",
+    "enlace": "https://t.me/+uKhpzIQ4IjFjZDgx"
+  },
+  {
+    "id": 20,
+    "nombre": "Gabriela Rivas",
+    "descripcion": "Comunidad oficial verificada de Gabriela Rivas. Espacio premium con novedades activas.",
+    "categoria": "Más Buscadas",
+    "pais": "HONDUREÑAS",
+    "imagen": "https://lh3.googleusercontent.com/d/1qMhCK5xXmsXQfXtR0-_Bosjj5qIgzlAR",
+    "enlace": "https://t.me/+8fTN-OJqrgYwNmRh"
+  },
+  {
+    "id": 21,
+    "nombre": "Rubi Sandoval",
+    "descripcion": "Canal premium exclusivo de Rubi Sandoval. El espacio oficial con novedades constantes.",
+    "categoria": "Más Buscadas",
+    "pais": "HONDUREÑAS",
+    "imagen": "https://lh3.googleusercontent.com/d/1q4m85443OI_4UjFTF59Zmn4ppDh3Znsg",
+    "enlace": "https://t.me/+enJWVflmbg0yMzUx"
+  },
+  {
+    "id": 22,
+    "nombre": "Daniella Cadenas",
+    "descripcion": "Únete para conocer todo el contenido verificado de Daniella Cadenas. Acceso premium.",
+    "categoria": "Más Buscadas",
+    "pais": "HONDUREÑAS",
+    "imagen": "https://lh3.googleusercontent.com/d/1ILtQqboySGQtdiyxdhcOElINnmXY4lY-",
+    "enlace": "https://t.me/+b58aNVWYD4djOGYx"
+  },
+  {
+    "id": 23,
+    "nombre": "Luz Rodriguez",
+    "descripcion": "Acceso premium inmediato al canal oficial de Luz Rodriguez con el material más buscado.",
+    "categoria": "Más Buscadas",
+    "pais": "HONDUREÑAS",
+    "imagen": "https://lh3.googleusercontent.com/d/1-2CGbPsqVFZZui3_R2qrOu4rJCYykxar",
+    "enlace": "https://t.me/+ptgVgP-KahkyNGIx"
+  },
+  {
+    "id": 24,
+    "nombre": "Yaritza Lopez",
+    "descripcion": "Comunidad VIP de Yaritza Lopez. Espacio verificado con actualizaciones.",
+    "categoria": "Más Buscadas",
+    "pais": "HONDUREÑAS",
+    "imagen": "https://lh3.googleusercontent.com/d/1l3idivIqE8UcwXaQiL7MFfx6w6fepDjN",
+    "enlace": "
